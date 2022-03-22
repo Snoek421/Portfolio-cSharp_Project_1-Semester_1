6d55e0f3dc3293f0186fe7984fd6104ae17abb62
Snoek421 <91285219+Snoek421@users.noreply.github.com>
2022-03-21 21:38:44 -0400

    uploaded

---
+++ Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+
+namespace A5CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string menuInput = null;
+            string nameStr;
+            string newNameStr = null;
+            int ageInt = 0;
+            int newAgeInt = 0;
+            string addressStr;
+            string newAddressStr = null;
+            Student student1;
+
+            student1 = new Student(); //create student1 as new empty student object to prevent crashes
+
+        Before_Menu:
+                        
+
+            if (menuInput == "B" || menuInput == "b")
+            {
+                student1.EditStudentInformation(newNameStr, newAgeInt, newAddressStr);
+                nameStr = student1.GetStudentName();
+                ageInt = student1.GetStudentAge();
+                addressStr = student1.GetStudentAddress();
+            }
+
+            Console.Clear();
+            Console.WriteLine("Please input a letter (from A to D) to select your action:");
+            Console.WriteLine("A - Add Student Record");
+            Console.WriteLine("B - Edit Student Record");
+            Console.WriteLine("C - Display the name, age, and address of the student");
+            Console.WriteLine("D - Exit the program");
+
+            //user input to choose action
+            menuInput = Console.ReadLine().Trim();
+
+            switch (menuInput)
+            {
+                case "A":
+                case "a":
+                    Console.Clear();
+                    if (!String.IsNullOrEmpty(student1.GetStudentName()))
+                    {
+                        Console.WriteLine("Student record (name) already exists. Press any key to go back to the menu.");
+                        Console.ReadKey();
+                        break;
+                    }
+                    else if (ageInt != 0)
+                    {
+                        Console.WriteLine("Student record (age) already exists. Press any key to go back to the menu.");
+                        Console.ReadKey();
+                        break;
+                    }
+                    else if (!String.IsNullOrEmpty(student1.GetStudentAddress()))
+                    {
+                        Console.WriteLine("Student record (address) already exists. Press any key to go back to the menu.");
+                        Console.ReadKey();
+                        break;
+                    }
+                    Console.WriteLine("Please input the student's name:");
+                    nameStr = Console.ReadLine().Trim(); //get name string and trim white space
+
+                Before_Age:
+                    Console.WriteLine("Please input the student's age (must not be 0):");
+                    string ageString = Console.ReadLine();
+                    if (String.IsNullOrEmpty(ageString) || ageString == "0")
+                    {
+                        Console.WriteLine("0 is not a valid age, press any key to try again.");
+                        Console.ReadKey();
+                        goto Before_Age;
+                    }
+                    else try
+                    {
+                        ageInt = Int32.Parse(ageString); //try to get age as int
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Your input was invalid, press any key to try again."); //if age input is not number then retry
+                        Console.ReadKey();
+                        goto Before_Age;
+                    }
+
+                    Console.WriteLine("Please input the student's address:");
+                    addressStr = Console.ReadLine(); //get address string
+
+
+                    student1 = new Student(nameStr, ageInt, addressStr); //reconstruct student1 instance with user input instead of empty values
+                    Console.WriteLine("Student record created. Press any key to return to menu.");
+                    Console.ReadKey();
+                    break;
+
+                case "B":
+                case "b":
+                    Console.Clear();
+                    Console.WriteLine(student1.DisplayStudentInformation());
+                    Console.WriteLine("Please input new information for each field.");
+                    //new info inputs
+                    Console.WriteLine("New name (leave blank to not change): ");
+                    newNameStr = Console.ReadLine().Trim(); //get new name and trim white space
+
+                New_Age_Input:
+                    Console.WriteLine("New age (enter 0 or leave blank to not change): "); //get new age
+                    string newAgeString = Console.ReadLine().Trim();
+                    if (String.IsNullOrEmpty(newAgeString))
+                    {
+                        newAgeInt = 0;
+                    }
+                    else try
+                    {
+                        newAgeInt = Int32.Parse(newAgeString); //try to get age as int
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Your input was invalid, press any key to try again."); //if age input is not number then retry
+                        Console.ReadKey();
+                        goto New_Age_Input;
+                    }
+
+                    Console.WriteLine("New address (leave blank to not change): ");
+                    newAddressStr = Console.ReadLine(); //get new address
+
+
+                    Console.WriteLine("Student information has been updated.");
+                    Console.WriteLine("Press any key to go back to the menu.");
+                    Console.ReadKey();
+
+                    break;
+                case "C":
+                case "c":
+                    Console.Clear();
+                    Console.WriteLine("Student 1 information is as follows:\n" + student1.DisplayStudentInformation());
+                    Console.WriteLine("Press any key to go back to the menu.");
+                    Console.ReadKey();
+
+                    break;
+                case "D":
+                case "d":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid selection, press any key to try again.");
+                    Console.ReadKey();
+                    break;
+            }
+            goto Before_Menu;
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A5CS
+{
+    public class Student
+    {
+        private string studentName;
+        private int studentAge;
+        private string studentAddress;
+
+        public Student() //default constructor
+        {
+            this.studentName = null;
+            this.studentAge = 0;
+            this.studentAddress = null;
+        }
+        public Student(string name, int age, string address)
+        {
+            studentName = name;
+            studentAge = age;
+            studentAddress = address;
+        }
+        public string DisplayStudentInformation()
+        {
+            return "Name: " + studentName + " \nAge: " + studentAge + "\nAddress: " + studentAddress;
+        }
+
+        public string GetStudentName()
+        {
+            return this.studentName;
+        }
+        public int GetStudentAge()
+        {
+            return this.studentAge;
+        }
+        public string GetStudentAddress()
+        {
+            return this.studentAddress;
+        }     
+        
+        public void EditStudentInformation(string name, int age, string address)
+        {
+            if (String.IsNullOrEmpty(name) || name == this.studentName)
+            {
+                Console.WriteLine("Name information will not be changed.");
+            }
+            else
+            {
+                this.studentName = name;
+            }
+            if (age == 0) //if entered age is 0 or if no input (also sends 0 to method) then no change
+            {
+                Console.WriteLine("Age information will not be changed.");
+            }
+            else
+            {
+                this.studentAge = age;
+            }
+            if (String.IsNullOrEmpty(address) || address == this.studentAddress)
+            {
+                Console.WriteLine("Address information will not be changed");
+            }
+            else
+            {
+                this.studentAddress = address;
+            }
+        }
+    }
+}
